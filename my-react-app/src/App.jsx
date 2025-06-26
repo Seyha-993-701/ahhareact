e12973d12b3f21f1bfb87e792f0dc38eb96bd535
Seyha-993-701 <pkg577542@gmail.com>
2025-06-26 15:09:46 +0700

    app bos anh

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+          <h2 className="text-2xl text-indigo-400">hello this is test tailwind</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
